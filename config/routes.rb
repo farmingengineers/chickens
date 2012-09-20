d5c5ba5584957bc3dbfb845a5deb185f992d69da
Matt Burke <spraints@gmail.com>
2012-09-19 21:52:50 -0400

    Make a menu for flocks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MattsChickens::Application.routes.draw do
   root :to => 'home#index'
-  resources :flocks
+  resources :flocks do
+    resources :feedings, :collections
+  end
   devise_for :users
 end
