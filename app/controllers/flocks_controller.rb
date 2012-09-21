48967759d2c2217b90ee8b002c7d397602f03c15
Matt Burke <spraints@gmail.com>
2012-09-21 00:44:45 -0400

    Edit a flock.

--- app/controllers/flocks_controller.rb
+++ app/controllers/flocks_controller.rb
@@ -1,7 +1,13 @@
 class FlocksController < ApplicationController
   before_filter :authenticate_user!
 
+  expose(:flocks) { current_user.flocks }
   expose(:flock)
+
+  def update
+    flock.save!
+    redirect_to root_path
+  end
 
   protected
 
